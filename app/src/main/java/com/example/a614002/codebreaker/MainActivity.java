2576c89b6817c635ac4ad4cb16ce8d8d570b9233
614002 <joshua.spencer010203@gmail.com>
2018-02-20 10:52:01 -0600

    Needs fixing

--- app/src/main/java/com/example/a614002/codebreaker/MainActivity.java
+++ app/src/main/java/com/example/a614002/codebreaker/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.a614002.codebreaker;
 
+import android.content.DialogInterface;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    Button btnStart = (Button) findViewById(R.id.status);
+        btnStart.setOnClickListener(new View.OnClickListener());
+            public void onClick(View arg0) {
+                // TODO Here I should generate the random code
+                // I created the toast to see if it worked
+                Toast.makeText(MainActivity.this, " start click", Toast.LENGTH_LONG).show();
+
+            }
 }
