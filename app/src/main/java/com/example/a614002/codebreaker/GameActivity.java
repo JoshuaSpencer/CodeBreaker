c72eff662a01064630fec99f3752856842312289
JoshuaSpencer <joshua.spencer010203@gmail.com>
2018-03-05 10:05:08 -0600

    Random Test

--- app/src/main/java/com/example/a614002/codebreaker/GameActivity.java
+++ app/src/main/java/com/example/a614002/codebreaker/GameActivity.java
@@ -2,6 +2,12 @@ package com.example.a614002.codebreaker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -9,5 +15,12 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+ //Random code test
+        Random rand = new Random();
+
+        int randomInt = rand.nextInt(50)+1;
+
+        TextView random = (TextView)findViewById(R.id.random);
+        random.setText(randomInt);
     }
 }
