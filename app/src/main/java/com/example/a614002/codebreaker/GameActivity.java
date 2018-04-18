00f4103cadc96dec0d58d2feb729cafd041feb1d
614002 <joshua.spencer010203@gmail.com>
2018-04-18 09:15:27 -0500

    Latest Code 4/18/2018

--- app/src/main/java/com/example/a614002/codebreaker/GameActivity.java
+++ app/src/main/java/com/example/a614002/codebreaker/GameActivity.java
@@ -3,28 +3,27 @@ package com.example.a614002.codebreaker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
+    TextView randomText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        randomText = (TextView)findViewById(R.id.randomTextView);
     }
+
 
     public void randomButton(View view){
         //Random code test
-        EditText random = findViewById(R.id.random);
-
-        Random rand = new Random();
-        int randomInt = rand.nextInt(50);
-        random.setText(randomInt);
+        Random randomNumber = new Random();
+        int randomInt = randomNumber.nextInt(50);
+        randomText.setText(randomInt);
 
     }
 }
